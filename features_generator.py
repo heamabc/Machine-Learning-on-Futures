4d04af5781f2c26ec7b314020430cd151f3ca77e
heamabc <32771919+heamabc@users.noreply.github.com>
2020-02-03 16:53:30 +0800

    Update features_generator.py

--- features_generator.py
+++ features_generator.py
@@ -4,8 +4,10 @@ import datetime
 
 def basis_momentum(sp, fp, periods):
     '''
+    Description
+    ----------
     basis momentum: the difference of r days culmulative return of nearby futures and farther futures.
-
+    
     Parameters
     ----------
     sp: pd.DataFrame
@@ -14,7 +16,7 @@ def basis_momentum(sp, fp, periods):
       first column is date, all other columns are farther futures price.
     periods: list
       list of periods of basis momentum to be calculated
-
+      
     Returns
     ----------
     pd.DataFrame
@@ -23,27 +25,24 @@ def basis_momentum(sp, fp, periods):
     output = pd.DataFrame()
     output["date"] = fp.iloc[:,0]
     sp = sp.iloc[:,1:]
-    fp = fp.iloc[:,1:]
-
-    for j in range(fp.shape[1]):
-        for r in periods:
-            tmplist = []
+    fp = fp.iloc[:,1:]    
+    
+    for r in periods:   
+        for j in range(fp.shape[1]):
+            tmp = np.where(sp.iloc[:,j].notnull() & fp.iloc[:,j].notnull(), 
+                           sp.iloc[:,j].astype(float)/sp.iloc[:,j].shift(r).astype(float) - fp.iloc[:,j].astype(float)/fp.iloc[:,j].shift(r).astype(float),
+                           None)
             
-            tmplist.extend(r * [None])
+            tmp[:r] = r * [None]
 
-            for i in range(r,fp.shape[0]):
-                if sp.iloc[i-r,j].isna() or fp.iloc[i-r,j].isna():
-                    tmplist.append(None)
-                    continue           
-
-                tmplist.append(float(sp.iloc[i,j])/float(sp.iloc[i-r,j]) - float(fp.iloc[i,j])/float(fp.iloc[i-r,j]))
-
-            output[fp.iloc[:,j].name + "_" + str(r)] = tmplist
+            output[fp.iloc[:,j].name + "_" + str(r)] = tmp
     return output
 
 
 def bias(data, periods):
     '''
+    Description
+    ----------
     bias: (price - r days moving average) / r days moving average * 100
     
     Parameters
@@ -52,7 +51,7 @@ def bias(data, periods):
       first column is date, all other columns are futures spot price.
     periods: list
       list of periods of bias to be calculated
-
+      
     Returns
     ----------
     pd.DataFrame
@@ -82,39 +81,85 @@ def bias(data, periods):
 
 
 def ln_price(data):
+    '''
+    Description
+    ----------
+    natural log of price
+    
+    Parameters
+    ----------
+    data: pd.DataFrame
+      first column is date, all other columns are futures spot price.
+
+    Returns
+    ----------
+    pd.DataFrame
+      first column is date, all other are ln price
+    '''
+    
     output = pd.DataFrame(data.iloc[:,0])
     output = pd.concat([output,np.log(data.iloc[:,1:])], axis=1)
     return output
 
 
 def return_signal_momentum(data, periods):
+    '''
+    Description
+    ----------
+    for r days, if there are more positive one-day return than negative one-day return, 1, else 0
+    
+    Parameters
+    ----------
+    data: pd.DataFrame
+      first column is date, all other columns are futures spot price.
+    periods: list
+      list of periods of return signal momentum to be calculated
+      
+    Returns
+    ----------
+    pd.DataFrame
+      first column is date, all other are return signal momentum
+    '''
+    
     output = pd.DataFrame()
     output["date"] = data.iloc[:,0]
     data = data.iloc[:,1:]
+    df_list = []
 
     data = data.pct_change()
-    for j in range(data.shape[1]):
-        product_name = data.iloc[:,j].name
-        for r in periods:
-            tmplist = []
-            for i in range(data.shape[0]):
-                if i < r:
-                    tmplist.append(None)
-                elif str(data.iloc[i-r,j]) == "nan":
-                    tmplist.append(None)
-                else:
-                    tmpdata = data.iloc[i-r:i,j]
-                    count = 0
-                    for d in tmpdata:
-                        if d >= 0:
-                            count += 1
-                    tmplist.append(count/r)
-            output[product_name + "_" + str(r)] = tmplist
-    return output
+    
+    def return_signal_momentum_function(r, series):
+        tmp = pd.Series(0)
+        
+        for i in range(r):
+            tmp += np.where(series.shift(r) >=0, 1, -1)
+            if i == r-1:
+                na_count = series.shift(r).isnan().sum()
+                
+        tmp.loc[tmp.iloc[:na_count].index] = None
+        tmp.loc[tmp.iloc[:r].index] = None
+        
+        tmp = np.where(tmp >= 0, 1,
+                 np.where(tmp.isnan()), None,
+                 0)
+        
+        return tmp
+        
+    for r in periods:
+        df_list.append(data.apply(lambda x:return_signal_momentum_function(r, x)).add_suffix('_' + str(r)))
+        
+    data = pd.concat(df_list, axis=1)
+        
+    return pd.concat([output, data], axis=1)
             
 
 def roll_rtn(sp, fp, ss):
     '''
+    Description
+    ------------
+    (log(sp) - log(fp))*365/diff
+    diff = settlement date - today
+    
     Parameters
     ------------
     sp: pd.DataFrame
@@ -130,15 +175,16 @@ def roll_rtn(sp, fp, ss):
       1 col = date, all other col are corresponding roll return
     '''
     output = pd.DataFrame()
+    ss = ss.iloc[:,1:]
     output['date'] = sp.iloc[:,0]
-    
-    diff = pd.DataFrame()
-    for j in range(1,ss.shape[1]):
-        tmplist = []
-        for i in range(ss.shape[0]):
-            delta = datetime.datetime.strptime(ss.iloc[i,1],"%m/%d/%Y") - datetime.datetime.strptime(ss.iloc[i,0],"%m/%d/%Y")
-            tmplist.append(delta.days)
-        diff[ss.iloc[:,j].name[:3]] = tmplist
+
+    def diff_function(series):
+        series = pd.to_datetime(series)
+        date = pd.to_datetime(output['date'])
+        
+        return (series - date).days
+            
+    diff = ss.apply(lambda x:diff_function(x))
     
     sp = sp.iloc[:,1:]
     fp = fp.iloc[:,1:]
@@ -148,6 +194,24 @@ def roll_rtn(sp, fp, ss):
 
 
 def rsi(data, periods):
+    '''
+    Description
+    ----------
+    for r days, if there are more positive one-day return than negative one-day return, 1, else 0
+    
+    Parameters
+    ----------
+    data: pd.DataFrame
+      first column is date, all other columns are futures spot price.
+    periods: list
+      list of periods of rsi to be calculated
+      
+    Returns
+    ----------
+    pd.DataFrame
+      first column is date, all other are rsi
+    '''
+    
     output = pd.DataFrame()
     output["date"] = data.iloc[:,0]
     data = data.iloc[:,1:]
@@ -183,7 +247,19 @@ def rsi(data, periods):
 
 def seasonality(data): 
     '''
+    Description
+    ----------
     Return the year-to-year percentage change
+    
+    Parameters
+    ----------
+    data: pd.DataFrame
+      first column is date, all other columns are futures spot price.
+      
+    Returns
+    ----------
+    pd.DataFrame
+      first column is date, all other are year-to-year pct change
     '''
     output = pd.DataFrame()
     output["date"] = data.iloc[:,0]
@@ -240,12 +316,28 @@ def seasonality(data):
 
 def signalizer(data, benchmark):
     '''
+    Description
+    ----------
     Binarize data according to the benchmark.
     Mark 1 if higher than benchmark, mark 0 if lower than benchmark
+    
+    Parameters
+    ----------
+    data: pd.DataFrame
+      first column is date, all other columns are values.
+    benchmark: int/float
+      benchmark value
+      
+    Returns
+    ----------
+    pd.DataFrame
+      first column is date, all other are Binarized data
     '''
     output = pd.DataFrame()
     output['date'] = data.iloc[:,0]
-    for j in range(1,data.shape[1]):
-        output[data.iloc[:,j].name] = data.iloc[:,j].apply(lambda x: 1 if x>=benchmark else 0 if x < benchmark else None)
+    
+    def signalizer_function(benchmark, series):
+        return series.apply(lambda x: 1 if x>=benchmark else 0 if x < benchmark else None)
         
-    return output
+    data = data.iloc[:,1:].apply(lambda x:signalizer_function(benchmark, series))
+    return pd.concat([output,data], axis=1)
